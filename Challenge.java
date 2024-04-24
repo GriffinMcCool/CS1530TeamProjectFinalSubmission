b80758d64841cee416338eea4ad4e8220239cb0a
GriffinMcCool <gmm92@pitt.edu>
2024-04-24 13:53:54 -0400

    Create Challenge.java

---
+++ Challenge.java
@@ -0,0 +1,15 @@
+public class Challenge{
+    String title;
+    String description;
+    String objective;
+    int difficulty;
+    String language;
+
+    public Challenge(String title, String description, String objective, int difficulty, String language) {
+        this.title = title;
+        this.description = description;
+        this.objective = objective;
+        this.difficulty = difficulty;
+        this.language = language;
+    }
+}
